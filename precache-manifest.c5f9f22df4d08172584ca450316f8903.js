b389a70612e3befc0d151ac6b9e7ee6c3e0230bc
NikitaUshenko <nikita-ushenko@mail.ru>
2019-07-03 20:02:51 +0300

    Updates

--- precache-manifest.c5f9f22df4d08172584ca450316f8903.js
+++ precache-manifest.c5f9f22df4d08172584ca450316f8903.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9f4c0541055d1cb80dc84d525e26cd92",
+    "revision": "48209dacc16a0bd483196c874fcb6f6d",
     "url": "/react_static-list-of-todos/index.html"
   },
   {
-    "revision": "b40e9ca27fda17825a70",
-    "url": "/react_static-list-of-todos/static/css/main.cb9e985a.chunk.css"
+    "revision": "2d3dd07f553d0e88b13f",
+    "url": "/react_static-list-of-todos/static/css/main.835417d6.chunk.css"
   },
   {
     "revision": "2315f74ce3c99316133d",
     "url": "/react_static-list-of-todos/static/js/2.3310f33a.chunk.js"
   },
   {
-    "revision": "b40e9ca27fda17825a70",
-    "url": "/react_static-list-of-todos/static/js/main.1d2983b2.chunk.js"
+    "revision": "2d3dd07f553d0e88b13f",
+    "url": "/react_static-list-of-todos/static/js/main.978545b5.chunk.js"
   },
   {
     "revision": "ea6f31786c2aed313835",
